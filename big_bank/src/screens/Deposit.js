3793fa5d847ed48ab3d00fc73c75be30a9da6b3a
MColbertPersevere <mcolbert@sharedservices.io>
2020-06-02 13:19:24 -0500

    Update Deposit.js

--- big_bank/src/screens/Deposit.js
+++ big_bank/src/screens/Deposit.js
@@ -1,25 +1,29 @@
 import React from 'react'
-import {Text, View, TextInput, Button, StyleSheet} from 'react-native'
+import {Text, View, TextInput, Button, StyleSheet, Keyboard} from 'react-native'
 import { Formik } from 'formik'
 import * as yup from 'yup'
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler'
+import { GlobalStyles } from '../styles/Global'
 
 const depositSchema = yup.object({
     amount: yup.string()
-            .required()
-            .test('is-num-1-250000', 'Money is only insured up to $250,000', (val) => {
-                return parseInt(val) < 250001 && parseInt(val) > 0; 
+            .required('Amount Required')
+            .test('is-num-1-10000', 'Minimum Deposit $1\nMaximum Deposit $10,000', (val) => {
+                return parseInt(val) < 10001 && parseInt(val) > 0; 
             }),
     memo: yup.string()
-            .required()
+            .required('Memo Required')
             .min(2)
 })
 
 const prevBalance = 100
 export default function Deposit () {
     return(
-        <View>
+        <View style={GlobalStyles.container}>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+
             <Formik
-                initialValues={{amount: 0, memo: '', balance: prevBalance}}
+                initialValues={{amount: '0', memo: '', balance: prevBalance}}
                 validationSchema={depositSchema}
                 onSubmit={(values, actions) => {
                     actions.resetForm();
@@ -31,16 +35,17 @@ export default function Deposit () {
                     <View>
                         <View style={styles.container}>
                             <Text style={styles.text}>Amount: </Text>
-                            <TextInput
-                                placeholder= 'Deposit Amount'
-                                onChangeText={props.handleChange('amount')}
-                                value={props.values.amount}
-                                keyboardType='numeric'
-                                onBlur={props.handleBlur('amount')}
-                                style={styles.input}
-                            />
+                                <TextInput
+                                    placeholder= 'Deposit Amount'
+                                    onChangeText={props.handleChange('amount')}
+                                    value={props.values.amount}
+                                    keyboardType='numeric'
+                                    onBlur={props.handleBlur('amount')}
+                                    style={styles.input}
+                                    
+                                />
                         </View>
-                            <Text>{props.touched.amount && props.errors.amount}</Text>
+                            <Text style={styles.errorText}>{props.touched.amount && props.errors.amount}</Text>
                             
                         <View style={styles.container}>
                             <Text style={styles.text}>Memo: </Text>
@@ -51,15 +56,17 @@ export default function Deposit () {
                                 onBlur={props.handleBlur('memo')}
                                 style={styles.input2}
                                 multiline
+                                minHeight={60}
                             />
                         </View>
-                            <Text>{props.touched.memo && props.errors.memo}</Text>
+                            <Text style={styles.errorText}>{props.touched.memo && props.errors.memo}</Text>
 
                         <Text value={props.values.balance}>New Balance: ${parseInt(props.values.amount) + parseInt(props.values.balance)}</Text>
                         <Button title='submit' onPress={props.handleSubmit} />
                     </View>
                 )}
             </Formik>
+            </TouchableWithoutFeedback>
         </View>
     )
 }
@@ -79,13 +86,20 @@ const styles = StyleSheet.create({
         borderColor:'#ddd',
         borderRadius: 6,
         fontSize: 18,
-        width: '40%'
+        width: '60%'
     },
     input2: {
         borderWidth: 1,
         borderColor:'#ddd',
         borderRadius: 6,
         fontSize: 18,
-        width: '50%'
+        width: '60%'
+    },
+    errorText: {
+        color: 'red',
+        fontWeight: 'bold',
+        marginBottom: 10,
+        marginTop: 6,
+        textAlign: 'center'
     }
 })
