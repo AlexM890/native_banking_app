815977092eda1d0f66d5faa4ba2dc11618fb9c8d
MColbertPersevere <mcolbert@sharedservices.io>
2020-06-02 11:41:48 -0500

    Update Deposit.js

--- big_bank/src/screens/Deposit.js
+++ big_bank/src/screens/Deposit.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import {Text, View, TextInput, Button, StyleSheet} from 'react-native'
 import { Formik } from 'formik'
+import * as yup from 'yup'
+
+const depositSchema = yup.object({
+    amount: yup.string()
+            .required()
+            .test('is-num-1-1000000', 'Deposit must be more than $0', (val) => {
+                return parseInt(val) < 1000001 && parseInt(val) > 0; 
+            }),
+    memo: yup.string()
+            .required()
+            .min(2)
+})
 
 const prevBalance = 100
 export default function Deposit () {
@@ -8,6 +20,7 @@ export default function Deposit () {
         <View>
             <Formik
                 initialValues={{amount: 0, memo: '', balance: prevBalance}}
+                validationSchema={depositSchema}
                 onSubmit={(values, actions) => {
                     actions.resetForm();
                     values.balance = parseInt(values.amount) + parseInt('100');
@@ -27,6 +40,7 @@ export default function Deposit () {
                                 style={styles.input}
                             />
                         </View>
+                            <Text>{props.touched.amount && props.errors.amount}</Text>
                             
                         <View style={styles.container}>
                             <Text style={styles.text}>Memo: </Text>
@@ -39,6 +53,7 @@ export default function Deposit () {
                                 multiline
                             />
                         </View>
+                            <Text>{props.touched.memo && props.errors.memo}</Text>
 
                         <Text value={props.values.balance}>New Balance: ${parseInt(props.values.amount) + parseInt(props.values.balance)}</Text>
                         <Button title='submit' onPress={props.handleSubmit} />
