8232cd6307248c70eead023b48e5b2c2dfd21a02
Kenneth007G <59965274+Kenneth007G@users.noreply.github.com>
2020-06-03 11:36:56 -0500

    update

--- big_bank/App.js
+++ big_bank/App.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import Withdrawal from "./src/screens/Withdrawal";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Withdrawal />
+      <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
