d88ddf88b403ed145ed17cbfc8184712f278b6ab
Kenneth007G <59965274+Kenneth007G@users.noreply.github.com>
2020-06-03 11:10:23 -0500

    made withddrawal screen

--- big_bank/App.js
+++ big_bank/App.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
@@ -8,12 +7,11 @@ export default function App() {
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
