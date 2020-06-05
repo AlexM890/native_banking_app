62711812525ccb7c4a7905ea07d87b15ae506699
AlexM890 <55465266+AlexM890@users.noreply.github.com>
2020-06-05 09:13:10 -0500

    Update Header.js

--- big_bank/src/shared/Header.js
+++ big_bank/src/shared/Header.js
@@ -30,10 +30,7 @@ const styles = StyleSheet.create({
     header: {
         flex: 1,
         flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems:"center",
-        backgroundColor: 'purple',
-        height:80,
+        justifyContent: 'space-around'
     },
 
 })
