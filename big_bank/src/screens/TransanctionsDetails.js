f16bbcc42bbad7e56ba59dbef1b6418beeb5d5a4
AlexM890 <55465266+AlexM890@users.noreply.github.com>
2020-06-05 15:02:53 -0500

    Update TransanctionsDetails.js

--- big_bank/src/screens/TransanctionsDetails.js
+++ big_bank/src/screens/TransanctionsDetails.js
@@ -1,10 +1,16 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState }from 'react'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
 import { GlobalStyles } from '../styles/Global'
 import Card from '../shared/Card'
+import { AntDesign } from '@expo/vector-icons'
+import { Formik } from 'formik'
+import { connect } from 'react-redux'
+import { editTransaction } from '../actions/transactionActions'
 
-const TransanctionsDetails = ({route}) => {
+const TransanctionsDetails = ({route, editTransaction}) => {
     const item = route.params
+    const [ edit, setEdit ] = useState(false)
+    const [ memo, setMemo ] = useState(item.memo)
     return (
         <View style={GlobalStyles.container}>
             <Card styles={styles.card}>
@@ -22,13 +28,53 @@ const TransanctionsDetails = ({route}) => {
             </Card>
             <Card styles={styles.card}>
                 <Text>Memo</Text>
-                <Text>{item.memo}</Text>
+                {
+                    !edit ? 
+                    <View style={styles.cardContainer}>
+                        <Text>{memo}</Text>
+                        <AntDesign name="edit" size={24} color="black" onPress={() => setEdit(true)}/>
+                    </View> :
+                    <View>
+                        <Formik
+                            initialValues={{memo: memo}}
+                            onSubmit={(values, actions) => {
+                                editTransaction(item.id, values.memo)
+                                setMemo(values.memo)
+                                setEdit(false)
+                                actions.resetForm();
+                            }}>
+                                {(props) => (
+                                    <View style={styles.editContainer}>
+                                        <View>
+                                            <Text style={styles.text}>Memo: </Text>
+                                            <TextInput
+                                                onChangeText={props.handleChange('memo')}
+                                                value={props.values.memo}
+                                                onBlur={props.handleBlur('memo')}
+                                                style={styles.input2}
+                                                multiline
+                                                minHeight={60}
+                                            />
+                                        </View>
+                                        <View>
+                                            <AntDesign name="checkcircleo" size={24} color="black" onPress={() => props.handleSubmit() }/>
+                                        </View>
+                                    </View>
+                                )}
+                        </Formik>
+                    </View>
+                }
             </Card>
         </View>
     )
 }
 
-export default TransanctionsDetails
+
+const mapDispatchToProps = (dispatch) => ({
+    editTransaction: (id, updates) => dispatch(editTransaction(id, updates))
+}) 
+
+export default connect(undefined, mapDispatchToProps)(TransanctionsDetails)
 
 const styles = StyleSheet.create({
     text:{
@@ -49,5 +95,15 @@ const styles = StyleSheet.create({
         shadowRadius: 2,
         marginHorizontal: 4,
         marginVertical: 6,
+    },
+    cardContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',  
+    },
+    editContainer:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        borderBottomColor: "black",
+        borderBottomWidth: 1,
     }
 })
